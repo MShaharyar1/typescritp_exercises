9825264c0afa0d2caa9a32b320a7d00a7ce5b36b
Muhammad Shaharyar <139736838+MShaharyar1@users.noreply.github.com>
2023-10-15 13:44:23 +0500

    Add files via upload

---
+++ q2.js
@@ -0,0 +1,8 @@
+"use strict";
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let a = "Eric ";
+console.log(a);
+let ba = a.toLowerCase();
+console.log(ba);
+let c = a.toUpperCase();
+console.log(c);
---
+++ q2.ts
@@ -0,0 +1,9 @@
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let a:string="Eric ";
+console.log(a)
+let ba=a.toLowerCase();
+console.log(ba)
+let c=a.toUpperCase();
+
+
+console.log(c)
